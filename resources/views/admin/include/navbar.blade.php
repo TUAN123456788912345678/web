38c6a9a750e1020fb9b7b5844b3bb276c4c3e726
tuan <vanminh291974@gmail.com>
2022-08-02 08:38:54 +0700

    first commit

--- resources/views/admin/include/navbar.blade.php
+++ resources/views/admin/include/navbar.blade.php
@@ -16,7 +16,29 @@
        <li class="nav-item">
         <a class="nav-link" href="#">Nick Game</a>
       </li>
-      
+      @guest
+                   
+               
+                                	 @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                 <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                       
+                                </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
     </ul>
   </div>
 </nav>
