9047ad90c4bab861f345e7c745a9d4cf6405caab
tuan <vanminh291974@gmail.com>
2022-08-09 09:08:27 +0700

    first commit

--- datawebacc/web_accgame-2.sql
+++ datawebacc/web_accgame-2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: localhost
--- Thời gian đã tạo: Th8 02, 2022 lúc 03:39 AM
+-- Thời gian đã tạo: Th8 09, 2022 lúc 04:06 AM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 8.1.6
 
@@ -24,6 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `blog`
+--
+
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` mediumtext NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `content` longblob NOT NULL,
+  `status` int(11) NOT NULL,
+  `updated_at` varchar(255) NOT NULL,
+  `created_at` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `category`
 --
 
@@ -33,7 +50,6 @@ CREATE TABLE `category` (
   `description` varchar(250) NOT NULL,
   `image` varchar(200) NOT NULL,
   `status` int(11) NOT NULL,
-  `order_category` int(11) DEFAULT NULL,
   `updated_at` varchar(100) NOT NULL,
   `created_at` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -42,12 +58,14 @@ CREATE TABLE `category` (
 -- Đang đổ dữ liệu cho bảng `category`
 --
 
-INSERT INTO `category` (`id`, `title`, `description`, `image`, `status`, `order_category`, `updated_at`, `created_at`) VALUES
-(1, 'tốc chiến', 'tốc chiến', 'thach28.jpg', 1, NULL, '2022-08-01 14:45:19', '1970-01-01 01:34:25'),
-(3, 'liên minh huyền thoại', 'liên minh huyền thoại', '1885.jpg', 0, NULL, '2022-08-01 14:41:07', '1970-01-01 00:00:05'),
-(4, 'fifa online 4', 'fifa online 4', 'ma73.jpg', 1, NULL, '2022-08-01 14:40:28', '1970-01-01 00:00:01'),
-(5, 'liên quân123', 'fghj', '414.jpg', 0, NULL, '2022-08-01 14:24:37', '1970-01-01 00:00:04'),
-(6, 'liên minh huyền thoại', 'liên minh huyền thoại', '1881.jpg', 0, NULL, '1970-01-01 00:00:06', '1970-01-01 00:00:06');
+INSERT INTO `category` (`id`, `title`, `description`, `image`, `status`, `updated_at`, `created_at`) VALUES
+(1, 'tốc chiến', 'tốc chiến', 'thach28.jpg', 1, '2022-08-01 14:45:19', '1970-01-01 01:34:25'),
+(3, 'liên minh huyền thoại', 'liên minh huyền thoại', '1885.jpg', 0, '2022-08-01 14:41:07', '1970-01-01 00:00:05'),
+(4, 'fifa online 4', 'fifa online 4', 'ma73.jpg', 1, '2022-08-01 14:40:28', '1970-01-01 00:00:01'),
+(5, 'liên quân123', 'fghj', '414.jpg', 0, '2022-08-01 14:24:37', '1970-01-01 00:00:04'),
+(6, 'liên minh huyền thoại1', '1231', '1881.jpg', 1, '2022-08-03 18:16:44', '1970-01-01 00:00:06'),
+(9, 'UYQEWUYRQWU', 'UDGSAIGSDI', '1524.jpg', 1, '1970-01-01 00:00:01', '1970-01-01 00:00:01'),
+(14, 'lol', '1', 'ya84.jpg', 0, '1970-01-01 00:00:01', '1970-01-01 00:00:01');
 
 -- --------------------------------------------------------
 
@@ -99,6 +117,14 @@ CREATE TABLE `password_resets` (
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `password_resets`
+--
+
+INSERT INTO `password_resets` (`email`, `token`, `created_at`) VALUES
+('v@gmail.com', '$2y$10$Y2gtHGV28k3WLXiefFgt0esE10daT121wvzjDLb1cG.tx/KOpT6he', '2022-08-08 17:29:30'),
+('vanminh291974@gmail.com', '$2y$10$rO5lM0f5Y/Rdn.cHZ7utX.VnYxfBKQ3XoAyXsMO8LY9YGvZyro/e6', '2022-08-08 17:30:29');
+
 -- --------------------------------------------------------
 
 --
@@ -116,6 +142,32 @@ CREATE TABLE `personal_access_tokens` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `slider`
+--
+
+CREATE TABLE `slider` (
+  `id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `updated_at` varchar(255) NOT NULL,
+  `created_at` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `slider`
+--
+
+INSERT INTO `slider` (`id`, `description`, `title`, `image`, `status`, `updated_at`, `created_at`) VALUES
+(2, 'suds', 'lol12', 't18.jpg', 0, '2022-08-07 14:17:41', '1970-01-01 00:00:01'),
+(3, 'fsdafasfd', 'lol', 'ya0.jpg', 0, '1970-01-01 00:00:01', '1970-01-01 00:00:01'),
+(4, '111111', 'lol1', 'a69.jpg', 0, '1970-01-01 00:00:01', '1970-01-01 00:00:01'),
+(5, 'safdsdfa', 'lol4', 'z38.jpg', 0, '1970-01-01 00:00:01', '1970-01-01 00:00:01');
 
 -- --------------------------------------------------------
 
@@ -139,11 +191,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'tuan', 'v@gmail.com', NULL, '$2y$10$FvhGXOB4UabrKb3bPktGMuKW.p4E7SPAEugL3DmNzQkZUIm5G.lUu', 'D2YIH2SgnKaEqYGhqmzHBt4IIStd2WZ2dpVBc2yYkZhyIeXpErDJYtCpaBQA', '2022-07-31 20:24:02', '2022-07-31 20:24:02');
+(1, 'tuan', 'v@gmail.com', NULL, '$2y$10$FvhGXOB4UabrKb3bPktGMuKW.p4E7SPAEugL3DmNzQkZUIm5G.lUu', 'OtJROf4jraZfuh7GS6qdthQnpSRmlWKPyQ2I7fDFKIFlrV2m7wJ7DRqfOmus', '2022-07-31 20:24:02', '2022-07-31 20:24:02'),
+(2, 'eee', 'e@gmail.com', NULL, '$2y$10$7S98VyblYI6STZgQH4vRaOvBwLz4g1brk6MJcnox8e9n6.XAvWcla', NULL, '2022-08-03 11:16:25', '2022-08-03 11:16:25'),
+(3, 'tuan', 'vanminh291974@gmail.com', NULL, '$2y$10$2V8hu2KxSmwouVcvRQPQz.9xo0b2K5HSrOYWurWzCaVKa54ptOWZS', 'yij2reQETLMIMo7NlOqkuPDUDrfRR9yh4yujpEOCtL19ON8oCDIOXrRkhbWT', '2022-08-08 17:30:01', '2022-08-08 17:30:01');
 
 --
 -- Chỉ mục cho các bảng đã đổ
 --
+
+--
+-- Chỉ mục cho bảng `blog`
+--
+ALTER TABLE `blog`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `category`
@@ -179,6 +239,12 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Chỉ mục cho bảng `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
@@ -190,10 +256,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT cho bảng `blog`
+--
+ALTER TABLE `blog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT cho bảng `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT cho bảng `failed_jobs`
@@ -214,10 +286,16 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT cho bảng `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
